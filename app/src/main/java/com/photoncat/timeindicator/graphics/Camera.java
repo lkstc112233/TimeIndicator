9ffae58ffe31f362f2b0286f3a4bbf18e51b9720
Photon cat <lkstc112233@users.noreply.github.com>
2019-01-02 03:38:50 -0500

    implemented free move function

--- app/src/main/java/com/photoncat/timeindicator/graphics/Camera.java
+++ app/src/main/java/com/photoncat/timeindicator/graphics/Camera.java
@@ -4,4 +4,8 @@ import com.photoncat.timeindicator.math.Vec3;
 
 public class Camera {
     public Vec3 position = new Vec3(0);
+
+    public void move(Vec3 direction) {
+        position.add(direction);
+    }
 }
