e14813690b8516179758a5c4df15ef4354c70b37
Photon cat <lkstc112233@users.noreply.github.com>
2019-01-02 03:39:14 -0500

    Added front vector and move forward method.

--- app/src/main/java/com/photoncat/timeindicator/graphics/Camera.java
+++ app/src/main/java/com/photoncat/timeindicator/graphics/Camera.java
@@ -3,9 +3,14 @@ package com.photoncat.timeindicator.graphics;
 import com.photoncat.timeindicator.math.Vec3;
 
 public class Camera {
+    private Vec3 front = new Vec3(0, 0, 1);
     public Vec3 position = new Vec3(0);
 
     public void move(Vec3 direction) {
         position.add(direction);
+    }
+
+    public void moveForward(float distance) {
+        position.add(Vec3.multiply(front, distance));
     }
 }
