35335415b557c5cc85e595703a1a887a6ba0b8a4
Photon cat <lkstc112233@users.noreply.github.com>
2019-01-02 02:44:12 -0500

    Added basic OpenGL support to the proj.

--- app/src/main/java/com/photoncat/timeindicator/ClockActivity.java
+++ app/src/main/java/com/photoncat/timeindicator/ClockActivity.java
@@ -1,12 +1,60 @@
 package com.photoncat.timeindicator;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class ClockActivity extends AppCompatActivity {
+    private static final String LOG_TAG = ClockActivity.class.getName();
+    private GLSurfaceView mGLView;
+
+    private boolean hasGLES30() {
+        ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        Log.e(LOG_TAG, "version: " + Integer.toHexString(configurationInfo.reqGlEsVersion));
+        return configurationInfo.reqGlEsVersion >= 0x30000;
+    }
+
+    private void initialize() {
+        if (hasGLES30()) {
+            mGLView = new GLSurfaceView(this);
+            mGLView.setEGLContextClientVersion(3);
+            mGLView.setPreserveEGLContextOnPause(true);
+            mGLView.setRenderer(new GLES30Renderer());
+        } else {
+            // Time to get a new phone, OpenGL ES 3.0 not supported.
+            throw new IllegalStateException("GLES 3.0 not supported.");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        initialize();
+        setContentView(mGLView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLView.onPause();
     }
 }
---
+++ app/src/main/java/com/photoncat/timeindicator/GLES30Renderer.java
@@ -0,0 +1,89 @@
+package com.photoncat.timeindicator;
+
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GLES30Renderer implements GLSurfaceView.Renderer {
+    private static final String LOG_TAG = GLES30Renderer.class.getName();
+    private boolean mFirstDraw;
+    private boolean mSurfaceCreated;
+    private int mWidth;
+    private int mHeight;
+    private long mLastTime;
+    private int mFPS;
+
+    public GLES30Renderer() {
+        mFirstDraw = true;
+        mSurfaceCreated = false;
+        mWidth = -1;
+        mHeight = -1;
+        mLastTime = System.currentTimeMillis();
+        mFPS = 0;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        Log.i(LOG_TAG, "Surface created.");
+        mSurfaceCreated = true;
+        mWidth = -1;
+        mHeight = -1;
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        if (!mSurfaceCreated && width == mWidth && height == mHeight) {
+            Log.i(LOG_TAG, "Surface changed but already handled.");
+            return;
+        }
+        // Android honeycomb has an option to keep the
+        // context.
+        String msg = "Surface changed width:" + width
+                + " height:" + height;
+        if (mSurfaceCreated) {
+            msg += ", context lost.";
+        } else {
+            msg += ".";
+        }
+        Log.i(LOG_TAG, msg);
+
+        mWidth = width;
+        mHeight = height;
+
+        onCreate(mWidth, mHeight, mSurfaceCreated);
+        mSurfaceCreated = false;
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        onDrawFrame(mFirstDraw);
+
+        mFPS++;
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - mLastTime >= 1000) {
+            mFPS = 0;
+            mLastTime = currentTime;
+        }
+
+        if (mFirstDraw) {
+            mFirstDraw = false;
+        }
+    }
+
+    public int getFPS() {
+        return mFPS;
+    }
+
+    public void onCreate(int width, int height,
+                         boolean contextLost) {
+        GLES30.glClearColor(0.1f, 0f, 0.3f, 1f);
+    }
+
+    public void onDrawFrame(boolean firstDraw) {
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT
+                | GLES30.GL_DEPTH_BUFFER_BIT);
+    }
+}
