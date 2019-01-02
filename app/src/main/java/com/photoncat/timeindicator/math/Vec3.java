138dd040d8eef0e0e5deb3c9dbef05d8680f021b
Photon cat <lkstc112233@users.noreply.github.com>
2019-01-02 03:24:46 -0500

    Added a vec3 class.

---
+++ app/src/main/java/com/photoncat/timeindicator/math/Vec3.java
@@ -0,0 +1,37 @@
+package com.photoncat.timeindicator.math;
+
+public class Vec3 {
+    private float x;
+    private float y;
+    private float z;
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public Vec3(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
