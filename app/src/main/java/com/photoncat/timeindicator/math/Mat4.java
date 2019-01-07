82be3a51663efdd4414a8c3459e3b0aabf6d483e
Photon cat <lkstc112233@users.noreply.github.com>
2019-01-07 02:28:00 -0500

    Added default value - identify matrix - to the mat4.

--- app/src/main/java/com/photoncat/timeindicator/math/Mat4.java
+++ app/src/main/java/com/photoncat/timeindicator/math/Mat4.java
@@ -3,6 +3,10 @@ package com.photoncat.timeindicator.math;
 public class Mat4 {
     private float[] data = new float[16];
 
+    public Mat4() {
+        data[0] = data[5] = data[10] = data[15] = 1.0F;
+    }
+
     public void set(int x, int y, float data) {
         this.data[x * 4 + y] = data;
     }
