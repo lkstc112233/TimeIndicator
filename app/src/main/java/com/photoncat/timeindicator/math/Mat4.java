4b522b648d9c7f92aa1a5aff8291e64918e5996f
Photon Cat <lkstc112233@gmail.com>
2019-01-06 19:08:08 -0500

    Implemented a single matrix

--- app/src/main/java/com/photoncat/timeindicator/math/Mat4.java
+++ app/src/main/java/com/photoncat/timeindicator/math/Mat4.java
@@ -1,4 +1,13 @@
 package com.photoncat.timeindicator.math;
 
 public class Mat4 {
+    private float[] data = new float[16];
+
+    public void set(int x, int y, float data) {
+        this.data[x * 4 + y] = data;
+    }
+
+    public float get(int x, int y) {
+        return data[x * 4 + y];
+    }
 }
